5ba6739cd93dae429975cab7b1cb9cc497a53183
unknown <rwh0003au@gmail.com>
2014-09-19 17:26:29 -0400

    changed index to php, further interface changes

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CANOPY | BLOG</title>
+	<link rel="stylesheet" href="home.css">
+	<link rel="stylesheet" href="Bootstrap/css/bootstrap.css">
+</head>
+<body>
+	<div class="page-wrapper container-fluid">
+		<div class="row">
+			<div class="left-nav col-md-3">
+				<div class="nav-logo">
+					<a href="#"><img src="home-layers/Logo.png" class="centered"></a>	
+				</div>
+				<div class="nav-description">
+					Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna alique. Ut enim ad minim veniam. quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				</div>
+				<div class="search-container">
+					<div class="search-bar">
+					</div>
+					<img src="home-layers/search_icon.png" id="search-icon">
+					<input type="text" name="search">
+				</div>
+				<div class="nav-menu">
+					<ul class="nav nav-stacked">
+						<li><a href="#">CLIENTS</a></li>
+						<li><a href="#">CAMPAIGNS</a></li>
+						<li><a href="#">TEAM</a></li>
+						<li><a href="#">ARCHIVES</a></li>
+						<ul class="nav nav-stacked archive-nav">
+							<li><a href="#">JULY 2014</a></li>
+							<li><a href="#">JUNE 2014</a></li>
+							<li><a href="#">MAY 2014</a></li>
+							<li><a href="#">APRIL 2014</a></li>
+						</ul>
+					</ul>
+				</div>
+			</div>
+			<div class="content-wrapper col-md-9">
+				<div class="top-background-image"></div>
+				<div class="content">
+					Test Content
+				</div>
+			</div>
+			<div class="row">
+				<div class="footer">
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="jQuery.js"></script>
+    <script src="Bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
